0503dfa6a09371719235867ab53529c70cd83864
Atharva Rasane <atra2377@colorado.edu>
2026-08-03 19:36:34 +0000

    Fix container SSH key ownership

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,7 @@ if [ "${START_SSHD:-0}" = "1" ]; then
     if [ -d /ssh-host ]; then
         install -d -m 0700 /root/.ssh
         cp -a /ssh-host/. /root/.ssh/
+        chown -R root:root /root/.ssh
         chmod 0700 /root/.ssh
         find /root/.ssh -type f -exec chmod 0600 {} +
         find /root/.ssh -type f -name '*.pub' -exec chmod 0644 {} +
